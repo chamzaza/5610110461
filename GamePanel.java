b066b600ce7b1fb4f1873aa2a80101819af19e98
chamzaza <chamzaza1754@hotmail.com>
2016-04-16 16:17:08 +0700

    #02 ใส่ยานสี่เหลี่ยมสีเขียว
    ใส่รูปยานกล่องสี่เหลี่ยมสีเขียว

--- GamePanel.java
+++ GamePanel.java
@@ -9,12 +9,31 @@ import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
-	
-	private BufferedImage bi;	
+
+	private BufferedImage bi;
 	Graphics2D big;
+	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 
 	public GamePanel() {
-		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);	
+		bi = new BufferedImage(400, 600, BufferedImage.TYPE_INT_ARGB);
+		big = (Graphics2D) bi.getGraphics();
+		big.setBackground(Color.BLACK);
+	}
+
+	public void updateGameUI(){
+		big.clearRect(0, 0, 400, 600);
+		big.setColor(Color.WHITE);
+		for(Sprite s : sprites){
+			s.draw(big);
+		}
+
+		repaint();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(bi, null, 0, 0);
 	}
 
 }
--- Main.java
+++ Main.java
@@ -6,10 +6,12 @@ public class Main {
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Space War");
 		GamePanel gp = new GamePanel();
+		SpaceShip v = new SpaceShip(180, 550, 20, 20);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
-		gp.setBackground(Color.BLACK);
+		GameEngine engine = new GameEngine(gp, v);
+		engine.start();
 		frame.add(gp);
 		frame.setVisible(true);
 	}
